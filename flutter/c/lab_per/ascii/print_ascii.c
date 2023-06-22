1ec72b20f1edd910cecca56549fb91122e45a69f
Harsh padariya <135210889+Harshpadariya51@users.noreply.github.com>
2023-06-22 13:02:47 +0530

    Add files via upload

---
+++ flutter/c/lab_per/ascii/print_ascii.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    for(i=65; i<=90; i++)
+    {
+        printf("%d: %c\n",i,i);
+    }
+    printf("\n\n");
+    for(i=97; i<=122; i++)
+    {
+        printf("%d: %c\n",i,i);
+    }
+
+}
